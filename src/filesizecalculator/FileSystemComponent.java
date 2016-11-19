126a4611193c4f8e285d46cff23bef671df8a9d0
mojtaba <mo.ahmadi@outlook.com>
2016-11-20 00:01:06 +0200

    The edition before upload

--- src/filesizecalculator/FileSystemComponent.java
+++ src/filesizecalculator/FileSystemComponent.java
@@ -2,35 +2,41 @@ import java.util.*;
 
 
 public abstract class FileSystemComponent {
-  String name;
+    String name;
 
-  public FileSystemComponent(String cName) {
-    name = cName;
-  }
-  public void addComponent(FileSystemComponent component)
-  throws CompositeException {
-    throw new CompositeException(
-      "Invalid Operation. Not Supported");
-  }
-  public FileSystemComponent getComponent(int componentNum)
-  throws CompositeException {
-    throw new CompositeException(
-      "Invalid Operation. Not Supported");
-  }
+    public FileSystemComponent(String cName) {
+        name = cName;
+    }
 
-  public abstract long getComponentSize();
-  
-  public int getNumberOfFiles()
-  throws CompositeException {
-    throw new CompositeException(
-      "Invalid Operation. Not Supported");
-  };
-  
-  public int getNumberOfSubdirectorie()
-  throws CompositeException {
-    throw new CompositeException(
-      "Invalid Operation. Not Supported");
-  };
+    public void addComponent(FileSystemComponent component)
+            throws CompositeException {
+        throw new CompositeException(
+                "Invalid Operation. Not Supported");
+    }
+
+    public FileSystemComponent getComponent(int componentNum)
+            throws CompositeException {
+        throw new CompositeException(
+                "Invalid Operation. Not Supported");
+    }
+
+    public abstract long getComponentSize();
+
+    public int getNumberOfFiles()
+            throws CompositeException {
+        throw new CompositeException(
+                "Invalid Operation. Not Supported");
+    }
+
+    ;
+
+    public int getNumberOfSubdirectorie()
+            throws CompositeException {
+        throw new CompositeException(
+                "Invalid Operation. Not Supported");
+    }
+
+    ;
 
 } // End of class FileSystemComponent
 
